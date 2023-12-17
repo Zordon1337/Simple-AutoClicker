83b51e1def6c4114d4cbdb3fd257317293a110d7
ZRD <ZRD@DESKTOP-04TCQOC>
2023-12-18 00:28:33 +0100

    Dodaj pliki projektów.

---
+++ AutoClicker/AutoClicker.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main(string args[])
+{
+    while (true)
+    {
+        if (GetAsyncKeyState(VK_SPACE))
+        {
+            Sleep(50);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 1280, 720, NULL, NULL);
+            mouse_event(MOUSEEVENTF_LEFTUP, 1280, 720, NULL, NULL);
+        }
+    }
+    return 0;
+}
